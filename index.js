1eb0ef607814e989fdaed563f15a121f74eb6e46
SanyTcheren <tcherenkovskiy@gmail.com>
2022-01-20 00:40:38 +0500

    reality start

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import { FILES, readSheet, writeSheet } from './helper/excelHelper.js';
 import * as cons from './helper/consoleHelper.js'
+import moment from 'moment'
 
-await cons.greating();
+// await cons.greating();
 
-await readSheet(FILES.source)
-await cons.setDrillingRig()
-await cons.setFieldBush()
-await cons.setMonth();
+// await readSheet(FILES.source)
+// await cons.setDrillingRig()
+// await cons.setFieldBush()
+// await cons.setMonth();
 
-await writeSheet(FILES.result);
+// await writeSheet(FILES.result);
 cons.goodbuy();
+
+const start = moment("2021-01-23 11");
+console.log(start.hour());
